794b6457211e08d1bcb1b0f4bad8e9992811fbf4
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-03-08 17:04:17 +0300

    Initial Commit

---
+++ AdaptiveUI/AdaptiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdaptiveUIApp.swift
+//  AdaptiveUI
+//
+//  Created by Danil Peregorodiev on 08.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct AdaptiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
